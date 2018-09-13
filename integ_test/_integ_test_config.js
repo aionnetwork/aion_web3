8f79dadd453b12662688cadf792435812f86ba36
aion-kelvin <kelvin@aion.network>
2018-09-13 17:14:36 -0400

    ported integ test of types.sol from aion_web3 0.0.1

---
+++ integ_test/_integ_test_config.js
@@ -0,0 +1,15 @@
+/*
+ * Configuration file for running integ tests; not an actual test.
+ */
+
+let config = {
+    /*
+    TEST_ACCT_ADDR: '',       // Populate with account with which deploy and send Txs
+    TEST_ACCT_PW: 'password', // Populate with password for test account
+    GAS: 0,                   // Populate with max gas for transactions (recommended: 4000000)
+    GAS_PRICE: 0,             // Populate with gas price (recommended: 10000000000)
+    */
+}
+module.exports = function() { 
+    return config;
+}();
---
+++ integ_test/contracts/README.md
@@ -0,0 +1,5 @@
+.abi and .bin files were are produced from their .sol counterpart using 
+Aion solc compiler: https://github.com/aionnetwork/aion_fastvm.
+
+Since there isn't a convenient way for the npm package to fetch and build
+Aion solc from source, including the .abi and .bin here for now.
