85e7c94c21732216b3640e4eb913b10d32f70a1a
aion-kelvin <kelvin@aion.network>
2018-09-14 09:56:27 -0400

    Integ test for send raw transaction with value but no data

--- integ_test/_integ_test_config.js
+++ integ_test/_integ_test_config.js
@@ -3,12 +3,20 @@
  */
 
 let config = {
-    /*
-    TEST_ACCT_ADDR: '',       // Populate with account with which deploy and send Txs
-    TEST_ACCT_PW: 'password', // Populate with password for test account
-    GAS: 0,                   // Populate with max gas for transactions (recommended: 4000000)
-    GAS_PRICE: 0,             // Populate with gas price (recommended: 10000000000)
-    */
+  // Account with which deploy and send Txs (needs to have Aions for tests to work)
+  TEST_ACCT_ADDR: '',       
+  // Sender account password
+  TEST_ACCT_PW: '', 
+  // Sender account private key (hex with leading 0x)
+  TEST_ACCT_PRIVKEY: '', 
+
+  // Account with which to receive Txs (No aion will be consumed for tests)
+  OTHER_TEST_ACCT_ADDR: '',
+
+  // Populate with max gas for transactions (recommended: 4000000)
+  GAS: 0,                   
+  // Populate with gas price (recommended: 10000000000)
+  GAS_PRICE: 0,             
 }
 module.exports = function() { 
     return config;
