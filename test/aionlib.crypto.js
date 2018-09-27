70f52f6ff680b010aa0df248c68d6b2a7680a250
William Zhai <you@example.com>
2018-09-27 15:08:18 -0400

    updated tests for aion crypto

--- test/aionlib.crypto.js
+++ test/aionlib.crypto.js
@@ -3,42 +3,29 @@ var assert = chai.assert;
 var hashutil = require('../packages/aion-lib/src/crypto.js');
 
 var inputVal = '123vnfsgj364hbdfh';
-var inputVal2 = '00000000000000000000000000000000';
-var blake128ResLength = 16;
-var blake256ResLength = 32; 
+var inputVal2 = '00000000000000000000000000000000'; 
 
 describe('blake2b128', function () {
 	it('should hash correctly', function () {
     	var res = hashutil.blake2b128(inputVal);
-    	assert.equal(res.length, blake128ResLength);
+        assert.equal(res.toString('hex'), '28208714050628dcb1118a16492a5dfc')
 	});
 
 	it('should hash correctly', function () {
     	var res = hashutil.blake2b128(inputVal2);
-		assert.equal(res.length, blake128ResLength);			
+        assert.equal(res.toString('hex'), 'ff0f22492f44bac4c4b30ae58d0e8daa')
     });
-
-    it('should hash consistantly', function () {
-    	var res1 = hashutil.blake2b128(inputVal);
-    	var res2 = hashutil.blake2b128(inputVal);
-    	assert.equal(res1.toString(), res2.toString(), 'hashes should b the same');
-    })
 });
 
 describe('blake2b256', function () {
 	it('should hash correctly', function () {
     	var res = hashutil.blake2b256(inputVal);
-    	assert.equal(res.length, blake256ResLength);
+        assert.equal(res.toString('hex'), '4e590cc616d8c3a0099cb70911c92e86da57a183bb253352bad4ec90d60f7c24')
 	});
 
 	it('should hash correctly', function () {
     	var res = hashutil.blake2b256(inputVal2);
-		assert.equal(res.length, blake256ResLength);			
+        assert.equal(res.toString('hex'), '89eb0d6a8a691dae2cd15ed0369931ce0a949ecafa5c3f93f8121833646e15c3')
     });
 
-    it('should hash consistantly', function () {
-    	var res1 = hashutil.blake2b256(inputVal);
-    	var res2 = hashutil.blake2b256(inputVal);
-    	assert.equal(res1.toString(), res2.toString(), 'hashes should b the same');
-    })
 });
