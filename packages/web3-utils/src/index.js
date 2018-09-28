54876ed9929d3d4be8d29358713b36e48228b238
William Zhai <you@example.com>
2018-09-28 15:18:40 -0400

    created aion-unit conversion implementations and functions

--- packages/web3-utils/src/index.js
+++ packages/web3-utils/src/index.js
@@ -23,7 +23,7 @@
 
 
 var _ = require('underscore');
-var ethjsUnit = require('ethjs-unit');
+var aionjsUnit = require('./aion-unit.js');
 var utils = require('./utils.js');
 var solidityBlake2b256 = require('./solidityBlake2b256.js');
 var randomHex = require('randomhex');
@@ -191,82 +191,72 @@ var asciiToHex = function(str) {
 
 
 /**
- * Returns value of unit in Wei
+ * Returns value of unit in nAmp
  *
  * @method getUnitValue
- * @param {String} unit the unit to convert to, default ether
- * @returns {BN} value of the unit (in Wei)
+ * @param {String} unit the unit to convert to, default aion
+ * @returns {BN} value of the unit (in nAmp)
  * @throws error if the unit is not correct:w
  */
 var getUnitValue = function (unit) {
-    unit = unit ? unit.toLowerCase() : 'ether';
-    if (!ethjsUnit.unitMap[unit]) {
-        throw new Error('This unit "'+ unit +'" doesn\'t exist, please use the one of the following units' + JSON.stringify(ethjsUnit.unitMap, null, 2));
+    unit = unit ? unit.toLowerCase() : 'aion';
+    if (!aionjsUnit.unitMap[unit]) {
+        throw new Error('This unit "'+ unit +'" doesn\'t exist, please use the one of the following units' + JSON.stringify(aionjsUnit.unitMap, null, 2));
     }
     return unit;
 };
 
 /**
- * Takes a number of wei and converts it to any other ether unit.
+ * Takes a number of nAmp and converts it to any other aion unit.
  *
  * Possible units are:
- *   SI Short   SI Full        Effigy       Other
- * - kwei       femtoether     babbage
- * - mwei       picoether      lovelace
- * - gwei       nanoether      shannon      nano
- * - --         microether     szabo        micro
- * - --         milliether     finney       milli
- * - ether      --             --
- * - kether                    --           grand
- * - mether
- * - gether
- * - tether
+ *   SI Short   SI Full        
+ * - namp       uaion    
+ * - uamp       maion      
+ * - mamp       caion  
+ * - amp        daion
+ * -            aion
  *
- * @method fromWei
+ *              
+ * @method fromNAmp
  * @param {Number|String} number can be a number, number string or a HEX of a decimal
- * @param {String} unit the unit to convert to, default ether
+ * @param {String} unit the unit to convert to, default aion
  * @return {String|Object} When given a BN object it returns one as well, otherwise a number
  */
-var fromWei = function(number, unit) {
+var fromNAmp = function(number, unit) {
     unit = getUnitValue(unit);
 
     if(!utils.isBN(number) && !_.isString(number)) {
         throw new Error('Please pass numbers as strings or BigNumber objects to avoid precision errors.');
     }
 
-    return utils.isBN(number) ? ethjsUnit.fromWei(number, unit) : ethjsUnit.fromWei(number, unit).toString(10);
+    return utils.isBN(number) ? aionjsUnit.fromNAmp(number, unit) : aionjsUnit.fromNAmp(number, unit).toString(10);
 };
 
 /**
- * Takes a number of a unit and converts it to wei.
+ * Takes a number of a unit and converts it to nAmp.
  *
  * Possible units are:
- *   SI Short   SI Full        Effigy       Other
- * - kwei       femtoether     babbage
- * - mwei       picoether      lovelace
- * - gwei       nanoether      shannon      nano
- * - --         microether     szabo        micro
- * - --         microether     szabo        micro
- * - --         milliether     finney       milli
- * - ether      --             --
- * - kether                    --           grand
- * - mether
- * - gether
- * - tether
+ *   SI Short   SI Full        
+ * - namp       uaion    
+ * - uamp       maion      
+ * - mamp       caion  
+ * - amp        daion
+ * -            aion
  *
- * @method toWei
+ * @method toNAmp
  * @param {Number|String|BN} number can be a number, number string or a HEX of a decimal
- * @param {String} unit the unit to convert from, default ether
+ * @param {String} unit the unit to convert from, default aion
  * @return {String|Object} When given a BN object it returns one as well, otherwise a number
  */
-var toWei = function(number, unit) {
+var toNAmp = function(number, unit) {
     unit = getUnitValue(unit);
 
     if(!utils.isBN(number) && !_.isString(number)) {
         throw new Error('Please pass numbers as strings or BigNumber objects to avoid precision errors.');
     }
 
-    return utils.isBN(number) ? ethjsUnit.toWei(number, unit) : ethjsUnit.toWei(number, unit).toString(10);
+    return utils.isBN(number) ? aionjsUnit.toNAmp(number, unit) : aionjsUnit.toNAmp(number, unit).toString(10);
 };
 
 /**
@@ -332,9 +322,9 @@ module.exports = {
     asciiToHex: asciiToHex,
     fromAscii: asciiToHex,
 
-    unitMap: ethjsUnit.unitMap,
-    toWei: toWei,
-    fromWei: fromWei,
+    unitMap: aionjsUnit.unitMap,
+    toNAmp: toNAmp,
+    fromNAmp: fromNAmp,
 
     padLeft: utils.leftPad,
     leftPad: utils.leftPad,
