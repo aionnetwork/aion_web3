e57279ee724155b87528d91cf0663773968da30a
aionWilliam <you@example.com>
2018-11-02 17:39:03 -0400

    updated integ test, using web3.eth.compileSolidity method to generate bin and abi for testing.

--- integ_test/contract.js
+++ integ_test/contract.js
@@ -8,10 +8,12 @@ let async = require('async')
 let Web3 = require('../')
 let should = require('should')
 let client = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545'))
+//let compiler = require("../packages/web3-eth");
 const crypto = require('crypto')
 
 let typesBinPath = path.join(__dirname, 'contracts', 'HelloWorld.bin')
 let typesAbiPath = path.join(__dirname, 'contracts', 'HelloWorld.abi')
+let typesSrcPath = path.join(__dirname, 'contracts', 'HelloWorld.sol')
 
 function random(from, to){
     return Math.floor(Math.random() * (to - from + 1)) + from
@@ -19,6 +21,7 @@ function random(from, to){
 
 function deployCt(ct, ctData, args, cb) { 
     console.log("SETUP: Deploying test contract...");
+
     return ct.deploy( {data: ctData, arguments: args} )
         .send()
         .on('error', err => { cb(err,null) })
@@ -31,7 +34,6 @@ function deployCt(ct, ctData, args, cb) {
         //.on('confirmation', (confirmationNumber, receipt) => { console.log(confirmationNumber, receipt) });
 }
 
-
 describe('contracts', () => {
   let opts = { 
       from: test_cfg.TEST_ACCT_ADDR,
@@ -39,38 +41,69 @@ describe('contracts', () => {
       gasPrice: test_cfg.GAS_PRICE,
   };
 
-  let typesBin
-  let typesAbi
   let ct;
   let ctInstAddress;
 
+  let web3 = new Web3(client);
+
+  let typesSrc;
+  let compiled;
+  let BIN;
+  let ABI;
+  let contract;
+
   let testNumber = 31337;
+  let source = fs.readFileSync(typesSrcPath, 'utf-8');
 
   before(done => {
     if( test_cfg.TEST_ACCT_ADDR.length == 0 ) { 
         done(Error("Error during setup.  No test account address was configured."));
     }
 
-    let steps = {
-      typesBin: async.apply(fs.readFile, typesBinPath),
-      typesAbi: async.apply(fs.readFile, typesAbiPath),
+    let steps = {    
+      typesSrc: async.apply(fs.readFile, typesSrcPath),
+
+      compiled: ['typesSrc', async.apply(function (res, cb){
+          let compiled2 = web3.eth.compileSolidity(source, function(err,res){ //necessary callback function
+            cb(null, res);
+          });
+
+      })],
+
+      BIN: ['compiled', async.apply(function(res, cb){
+          let bin = res.compiled['HelloWorld'].code;
+
+          cb(null, bin);
+      })], 
+
+      ABI: ['compiled', async.apply(function(res, cb){
+          let abi = res.compiled['HelloWorld'].info.abiDefinition;
+
+          cb(null, abi);
+      })],
+
+      //-------------------------------------------------------------
       unlock: async.apply(client.eth.personal.unlockAccount, 
                           test_cfg.TEST_ACCT_ADDR , 
                           test_cfg.TEST_ACCT_PW),
-      contract: ['typesAbi',async.apply(function (results,cb) {
-          let ct = new client.eth.Contract(JSON.parse(results.typesAbi), opts);
+
+      contract: ['ABI', async.apply(function (results,cb) {
+          let ct = new client.eth.Contract(results.ABI, opts);
           cb(null, ct);
       })],
-      deploy: ['unlock', 'typesBin', async.apply(function (res,cb) {
+
+      deploy: ['unlock', 'BIN', async.apply(function (res,cb) {
           let deployedTo;
-          deployCt(res.contract, res.typesBin.toString('utf8'), [testNumber], cb)
+          deployCt(res.contract, res.BIN.toString('utf8'), [testNumber], cb)
               .catch(err => {
                   console.error("error", err);
                   return done(err);
               });
-      })]
+      })],
     }
+
     async.auto(steps, (err, res) => {
+
       if (err !== null && err !== undefined) {
         console.error('error reading bin & abi', err)
         return done(err)
@@ -79,17 +112,15 @@ describe('contracts', () => {
       if(! res.unlock ) { 
         return done(new Error("can't unlock"));
       }
-
-      typesBin = res.typesBin.toString('utf8')
-      typesAbi = JSON.parse(res.typesAbi)
-      ct = res.ct;
+      
       ctInstAddress = res.deploy.contractAddress;
 
-      ct = new client.eth.Contract(typesAbi, ctInstAddress);
+      ct = new client.eth.Contract(res.ABI, ctInstAddress);
 
-      done()
+      done();
     })
   })
+  
 
   it('contract method call with event', done => {
       ct.methods.sayHello()
@@ -103,5 +134,4 @@ describe('contracts', () => {
               done(err);
           });
   });
-
 })
