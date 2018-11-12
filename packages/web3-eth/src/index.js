dec1978990caa8a9652e24338b8d7ad593259571
aionWilliam <william@aion.network>
2018-11-12 12:57:00 -0500

    commented out uncle methods

--- packages/web3-eth/src/index.js
+++ packages/web3-eth/src/index.js
@@ -49,17 +49,17 @@ var transactionFromBlockCall = function (args) {
     return (_.isString(args[0]) && args[0].indexOf('0x') === 0) ? 'eth_getTransactionByBlockHashAndIndex' : 'eth_getTransactionByBlockNumberAndIndex';
 };
 
-var uncleCall = function (args) {
-    return (_.isString(args[0]) && args[0].indexOf('0x') === 0) ? 'eth_getUncleByBlockHashAndIndex' : 'eth_getUncleByBlockNumberAndIndex';
-};
+// var uncleCall = function (args) {
+//     return (_.isString(args[0]) && args[0].indexOf('0x') === 0) ? 'eth_getUncleByBlockHashAndIndex' : 'eth_getUncleByBlockNumberAndIndex';
+// };
 
 var getBlockTransactionCountCall = function (args) {
     return (_.isString(args[0]) && args[0].indexOf('0x') === 0) ? 'eth_getBlockTransactionCountByHash' : 'eth_getBlockTransactionCountByNumber';
 };
 
-var uncleCountCall = function (args) {
-    return (_.isString(args[0]) && args[0].indexOf('0x') === 0) ? 'eth_getUncleCountByBlockHash' : 'eth_getUncleCountByBlockNumber';
-};
+// var uncleCountCall = function (args) {
+//     return (_.isString(args[0]) && args[0].indexOf('0x') === 0) ? 'eth_getUncleCountByBlockHash' : 'eth_getUncleCountByBlockNumber';
+// };
 
 
 var Eth = function Eth() {
@@ -253,14 +253,14 @@ var Eth = function Eth() {
             inputFormatter: [formatter.inputBlockNumberFormatter, function (val) { return !!val; }],
             outputFormatter: formatter.outputBlockFormatter
         }),
-        new Method({
-            name: 'getUncle',
-            call: uncleCall,
-            params: 2,
-            inputFormatter: [formatter.inputBlockNumberFormatter, utils.numberToHex],
-            outputFormatter: formatter.outputBlockFormatter,
+        // new Method({
+        //     name: 'getUncle',
+        //     call: uncleCall,
+        //     params: 2,
+        //     inputFormatter: [formatter.inputBlockNumberFormatter, utils.numberToHex],
+        //     outputFormatter: formatter.outputBlockFormatter,
 
-        }),
+        // }),
         new Method({
             name: 'getBlockTransactionCount',
             call: getBlockTransactionCountCall,
@@ -268,13 +268,13 @@ var Eth = function Eth() {
             inputFormatter: [formatter.inputBlockNumberFormatter],
             outputFormatter: utils.hexToNumber
         }),
-        new Method({
-            name: 'getBlockUncleCount',
-            call: uncleCountCall,
-            params: 1,
-            inputFormatter: [formatter.inputBlockNumberFormatter],
-            outputFormatter: utils.hexToNumber
-        }),
+        // new Method({
+        //     name: 'getBlockUncleCount',
+        //     call: uncleCountCall,
+        //     params: 1,
+        //     inputFormatter: [formatter.inputBlockNumberFormatter],
+        //     outputFormatter: utils.hexToNumber
+        // }),
         new Method({
             name: 'getTransaction',
             call: 'eth_getTransactionByHash',
