c28b1242e9401c3851fa2b6125dde575e289b69a
aionWilliam <william@aion.network>
2018-11-12 12:57:42 -0500

    commented out all the tests for getUncle and uncle methods

--- test/eth_methods.js
+++ test/eth_methods.js
@@ -16,9 +16,9 @@ describe('eth', function() {
         u.methodExists(eth, 'call');
         u.methodExists(eth, 'getBlock');
         u.methodExists(eth, 'getTransaction');
-        u.methodExists(eth, 'getUncle');
+        //u.methodExists(eth, 'getUncle');
         u.methodExists(eth, 'getBlockTransactionCount');
-        u.methodExists(eth, 'getBlockUncleCount');
+        //u.methodExists(eth, 'getBlockUncleCount');
         u.methodExists(eth, 'subscribe');
         u.methodExists(eth, 'Contract');
 
@@ -30,6 +30,9 @@ describe('eth', function() {
         u.methodExists(eth, 'getBlockNumber');
 
         u.methodExists(eth, 'getProtocolVersion');
+
+        u.methodExists(eth, 'getCompilers');
+        u.methodExists(eth, 'compileSolidity');
 
         u.methodExists(eth, 'setProvider');
         u.propertyExists(eth, 'givenProvider');
