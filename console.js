e25f4594c4f3f5cf7b75ec39acf949dfaafb8080
jenniferwang703 <jennifer@aion.network>
2019-01-15 12:16:48 -0500

    add web socket and ipc to console.js

--- console.js
+++ console.js
@@ -4,24 +4,38 @@ var url = require('url');
 var Web3 = require('./packages/web3');
 
 try {
+  var promptPrefix,web3;
   if (process.argv[2]) {
-    var promptPrefix;
-    const myURL = url.parse(process.argv[2]);
-    if(myURL.protocol == 'https:' || myURL.protocol == 'http:') {
-      endpoint = process.argv[2];
-      console.log('Connected to', endpoint);
-      promptPrefix = myURL.host;
-    } else {
-      throw 'Invalid address, make sure your node address follow the format "protocol://host:port"(e.g. https://aion.node:8545). Supported protocols: http, https';
-    }
-  } else {
-    endpoint = 'http://127.0.0.1:8545';
-    console.log('Connected to default', endpoint);
-    promptPrefix = endpoint;
+    var endpoint = process.argv[2];
+    const myURL = url.parse(endpoint);
+
+    switch(myURL.protocol){
+    	case 'https:':
+    	case 'http:':
+    		web3 = new Web3(new Web3.providers.HttpProvider(endpoint));
+    		break;
+    	case 'ws:':
+    		web3 = new Web3(new Web3.providers.WebsocketProvider(endpoint));
+    		break;
+      	case 'ipc:':
+        	web3 = new Web3(new Web3.providers.IpcProvider(myURL.path,require('net')));
+        	break;
+    	default:
+    		throw 'Invalid address, make sure your node address follow the format "protocol://host:port"(e.g. https://aion.node:8545). Supported protocols: http, https and ws';
+      }
+      promptPrefix = myURL.host || myURL.path;
+  }else{
+        endpoint = 'http://127.0.0.1:8545';
+        console.log("connected to default", endpoint);
+        web3 = new Web3(new Web3.providers.HttpProvider(endpoint));
+        promptPrefix = endpoint;
   }
+    
+
 
   var context = repl.start(promptPrefix + '> ').context;
-  context.web3 = new Web3(new Web3.providers.HttpProvider(endpoint));
+  //context.web3 = new Web3(new Web3.providers.HttpProvider(endpoint));
+  context.web3 = web3;
   context.eth = context.web3.eth;
   context.personal = context.web3.personal;
 } catch(error) {
