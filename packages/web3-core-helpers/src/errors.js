48ba1d8d2d9f48975accdd72581c90652fe1e6d0
dev-rohan <Rohan@nuco.io>
2019-03-06 10:47:11 -0400

    Refined Errors in web3-core-helpers/src/formatters.js

--- packages/web3-core-helpers/src/errors.js
+++ packages/web3-core-helpers/src/errors.js
@@ -26,7 +26,7 @@
 "use strict";
 
 module.exports = {
-    ErrorResponse: function (result) {
+    ErrorResponse: function(result) {
         var message = !!result && !!result.error && !!result.error.message ? result.error.message : JSON.stringify(result);
         if (result && result.error && result.error.data)
             message += "..." + result.error.data;
@@ -35,13 +35,13 @@ module.exports = {
     MissingCoreProperty: function(property) {
         return new Error('When creating a method you need to provide at least the "' + property + '" property.')
     },
-    InvalidNumberOfParams: function (got, expected, method) {
+    InvalidNumberOfParams: function(got, expected, method) {
         return new Error('Invalid number of parameters for "'+ method +'". Got '+ got +' expected '+ expected +'!');
     },
-    InvalidConnection: function (host){
+    InvalidConnection: function(host) {
         return new Error('CONNECTION ERROR: Couldn\'t connect to node '+ host +'.');
     },
-    InvalidProvider: function () {
+    InvalidProvider: function() {
         return new Error('Provider not set or invalid');
     },
     InvalidInstantiation: function() {
@@ -51,7 +51,28 @@ module.exports = {
         var message = !!result && !!result.error && !!result.error.message ? result.error.message : 'Invalid JSON RPC response: ' + JSON.stringify(result);
         return new Error(message);
     },
-    ConnectionTimeout: function (ms){
+    ConnectionTimeout: function(ms) {
         return new Error('CONNECTION TIMEOUT: timeout of ' + ms + ' ms achived');
+    },
+    TxInputFormatterDataInputError: function() {
+        return new Error('You can\'t have "data" and "input" as properties of transactions at the same time, please use either "data" or "input" instead.');
+    },
+    InputTransactionFormatterUndefinedFromField: function() {
+        return new Error('The send transactions "from" field must be defined!');
+    },
+    InputAddressFormatterInvalidAddress: function(address) {
+        return new Error('Provided address "'+ address +'" is invalid, the capitalization checksum test failed, or its an indrect IBAN address which can\'t be converted.')
+    },
+    OutputTransactionReceiptFormatterInvalidReceipt: function(receipt) {
+        return new Error('Received receipt is invalid: ' + receipt);
+    },
+    TxInputFormatterDataHexError: function() {
+        return new Error('The data field must be HEX encoded data.');
+    },
+    InputZipfileBase64FormatterInvalidArray: function() {
+        return new Error('This must be a list of .sol filepaths');
+    },
+    InputZipfileBase64FormatterInvalidContract: function() {
+        return new Error('One of the provided filepaths does not include a valid .sol file');
     }
 };
